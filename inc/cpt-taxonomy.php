6ec261651f469a36e7c086a5dd631be714303e5f
mamin24 <mamin9@my.bcit.ca>
2024-06-05 10:58:36 -0700

    changed a lot

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -49,6 +49,54 @@ function fwd_register_custom_post_types() {
     );
 
     register_post_type( 'fwd-student', $args );
+
+    $labels = array(
+        'name'                  => _x( 'Staffs', 'post type general name' ),
+        'singular_name'         => _x( 'Staffs', 'post type singular name'),
+        'menu_name'             => _x( 'Staffs', 'admin menu' ),
+        'name_admin_bar'        => _x( 'Staffs', 'add new on admin bar' ),
+        'add_new'               => _x( 'Add New', 'Staff' ),
+        'add_new_item'          => __( 'Add New Staff' ),
+        'new_item'              => __( 'New Staff' ),
+        'edit_item'             => __( 'Edit Staff' ),
+        'view_item'             => __( 'View Staff' ),
+        'all_items'             => __( 'All Staff' ),
+        'search_items'          => __( 'Search Staff' ),
+        'parent_item_colon'     => __( 'Parent Staffs:' ),
+        'not_found'             => __( 'No staffs found.' ),
+        'not_found_in_trash'    => __( 'No staffs found in Trash.' ),
+        'archives'              => __( 'Staff Archives'),
+        'insert_into_item'      => __( 'Insert into staff'),
+        'uploaded_to_this_item' => __( 'Uploaded to this staff'),
+        'filter_item_list'      => __( 'Filter staffs list'),
+        'items_list_navigation' => __( 'Staffs list navigation'),
+        'items_list'            => __( 'Staffs list'),
+        'featured_image'        => __( 'Staff featured image'),
+        'set_featured_image'    => __( 'Set staff featured image'),
+        'remove_featured_image' => __( 'Remove staff featured image'),
+        'use_featured_image'    => __( 'Use as featured image'),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'staffs' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'menu_icon'          => 'dashicons-archive',
+        'supports'           => array( 'title', 'thumbnail', 'editor' ),
+    );
+
+    register_post_type( 'fwd-staff', $args );
 }
 add_action( 'init', 'fwd_register_custom_post_types' );
 
@@ -81,6 +129,33 @@ function fwd_register_taxonomies() {
         'rewrite'           => array( 'slug' => 'student-categories' ),
     );
     register_taxonomy( 'fwd-student-category', array( 'fwd-student' ), $args );
+
+    $labels = array(
+        'name'              => _x( 'Staff Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Staff Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Staff Categories' ),
+        'all_items'         => __( 'All Staff Category' ),
+        'parent_item'       => __( 'Parent Staff Category' ),
+        'parent_item_colon' => __( 'Parent Staff Category:' ),
+        'edit_item'         => __( 'Edit Staff Category' ),
+        'view_item'         => __( 'View Staff Category' ),
+        'update_item'       => __( 'Update Staff Category' ),
+        'add_new_item'      => __( 'Add New Staff Category' ),
+        'new_item_name'     => __( 'New Staff Category Name' ),
+        'menu_name'         => __( 'Staff Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'staff-categories' ),
+    );
+    register_taxonomy( 'fwd-staff-category', array( 'fwd-staff' ), $args );
 }
 add_action( 'init', 'fwd_register_taxonomies');
 
